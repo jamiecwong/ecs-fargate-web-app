2ada8f3e484145c16194120652fc7722502359ec
Jamie Wong <jamiewong@bulb.co.uk>
2021-10-13 11:21:55 +0100

    refactor iam

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -39,3 +39,48 @@ resource "aws_cloudwatch_log_group" "this" {
   retention_in_days = 1
   tags              = { env = var.env }
 }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${local.cluster_name}-ecsTaskRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${local.cluster_name}-ecsTaskExecutionRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
